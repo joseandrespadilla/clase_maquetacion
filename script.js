462ade707aa90e5e5e59cb92ee68c6109e662993
Ann2123 <fortisregina@gmail.com>
2023-11-18 16:58:30 -0400

    Reproductor de musica

---
+++ script.js
@@ -0,0 +1,62 @@
+
+let tiponumero = 7.644353458766
+let tipecadenadetexto = 'Hola Mundo'
+let tipobooleano = true
+let tipoNulo = null
+let tipolista = [
+7,
+60,
+54,
+3,
+0,
+23,
+"hjfgu",
+"kghjk",
+"kjhoui",
+undefined
+
+ ]
+
+ let tipoObjeto = {
+    0: 7,
+    1: 60, 
+    100: 1914,
+    name: "Jose",
+    password: "1234",
+    address: {
+    city: "Valencia",
+    country: "Venezuela",
+    }
+}
+let tipoLista = [
+    tipoObjeto,
+    tipoObjeto,
+    tipoObjeto,
+    tipoObjeto,
+    tipoObjeto,
+]
+if (5 === 8) {
+    console.log("Se cumplio la funcion")
+}  
+
+else{
+    console.log("Lo otro no se cumple")
+}
+5 === 8
+? console.log ("Se cumplio la condicion")
+: console.log ("lo otro no se cumplio")
+
+
+
+function MiFuncion() {
+    console.lopg('Hola')
+console.log('Bienvenidos')
+console.log ('a venezuela')
+}
+let MiFuncionFlecha =
+() =>  {
+
+}
+console.log(
+    MiFuncionFlecha()
+    )
